a8e09630ca90bc1e6380c84802abef204199c2bf
Nguyen Gia Huy (RBVH/EDA15) <guu7hc@bosch.com>
2020-11-17 16:32:19 +0700

    Initial commit

---
+++ MVVM_Demo/Models/EmployeeServiceEF.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MVVM_Demo.Models
+{
+    public class EmployeeServiceEF
+    {
+        private static List<EmployeeDTO> ObjEmployeesList;
+
+        public EmployeeServiceEF()
+        {
+        }
+
+        public List<EmployeeDTO> GetAll()
+        {
+            return ObjEmployeesList;
+        }
+
+        public bool Add(EmployeeDTO objNewEmployee)
+        {
+            if (objNewEmployee.Age < 21 || objNewEmployee.Age > 58)
+                throw new ArgumentException("Invalid age limit for employee");
+
+            ObjEmployeesList.Add(objNewEmployee);
+            return true;
+        }
+
+        public bool Update(EmployeeDTO objEmployeeToUpdate)
+        {
+            bool isUpdated = false;
+
+            foreach (var employee in ObjEmployeesList)
+            {
+                if (employee.Id == objEmployeeToUpdate.Id)
+                {
+                    employee.Name = objEmployeeToUpdate.Name;
+                    employee.Age = objEmployeeToUpdate.Age;
+                    isUpdated = true;
+                    break;
+                }
+            }
+
+            return isUpdated;
+        }
+
+        public bool Delete(int id)
+        {
+            bool isDeleted = false;
+
+            var employeeToDelete = ObjEmployeesList.FirstOrDefault(e => e.Id == id);
+
+            if (employeeToDelete != null)
+            {
+                ObjEmployeesList.Remove(employeeToDelete);
+                isDeleted = true;
+            }
+
+            return isDeleted;
+        }
+
+        public EmployeeDTO Search(int id)
+        {
+            return ObjEmployeesList.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
---
+++ MVVM_Demo/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,199 @@
+﻿using MVVM_Demo.Commands;
+using MVVM_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace MVVM_Demo.ViewModels
+{
+    public class EmployeeViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        EmployeeService ObjEmployeeService;
+
+        public EmployeeViewModel()
+        {
+            ObjEmployeeService = new EmployeeService();
+            LoadData();
+            CurrentEmployee = new EmployeeDTO();
+            saveCommand = new RelayCommand(Save);
+            searchCommand = new RelayCommand(Search);
+            updateCommand = new RelayCommand(Update);
+            deleteCommand = new RelayCommand(Delete);
+        }
+
+        #region Operations
+
+        private ObservableCollection<EmployeeDTO> employeesList;
+
+        public ObservableCollection<EmployeeDTO> EmployeesList
+        {
+            get { return employeesList; }
+            set { employeesList = value; OnPropertyChanged("EmployeesList"); }
+        }
+
+        private void LoadData()
+        {
+            EmployeesList = new ObservableCollection<EmployeeDTO>(ObjEmployeeService.GetAll());
+        }
+
+        #endregion
+
+        private EmployeeDTO currentEmployee;
+
+        public EmployeeDTO CurrentEmployee
+        {
+            get { return currentEmployee; }
+            set { currentEmployee = value; OnPropertyChanged("CurrentEmployee"); }
+        }
+
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value; OnPropertyChanged("Message"); }
+        }
+
+        #region Save Command
+
+        private RelayCommand saveCommand;
+
+        public RelayCommand SaveCommand
+        {
+            get { return saveCommand; }
+        }
+
+        public void Save()
+        {
+            try
+            {
+                var IsSaved = ObjEmployeeService.Add(CurrentEmployee);
+
+                if (IsSaved)
+                {
+                    Message = "Employee saved";
+                    LoadData();
+                }
+                else
+                    Message = "Save failed";
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+            }
+        }
+
+        #endregion
+
+        #region Search
+
+        private RelayCommand searchCommand;
+
+        public RelayCommand SearchCommand
+        {
+            get { return searchCommand; }
+        }
+
+        public void Search()
+        {
+            try
+            {
+                var ObjEmployee = ObjEmployeeService.Search(currentEmployee.Id);
+
+                if (ObjEmployee != null)
+                {
+                    CurrentEmployee.Name = ObjEmployee.Name;
+                    CurrentEmployee.Age = ObjEmployee.Age;
+                }
+                else
+                    Message = "Employee not found";
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+            }
+        }
+
+        #endregion
+
+        #region Update
+
+        private RelayCommand updateCommand;
+
+        public RelayCommand UpdateCommand
+        {
+            get { return updateCommand; }
+        }
+
+        public void Update()
+        {
+            try
+            {
+                var IsUpdated = ObjEmployeeService.Update(CurrentEmployee);
+
+                if (IsUpdated)
+                {
+                    Message = "Employee Updated";
+                    LoadData();
+                }
+                else
+                {
+                    Message = "Update failed";
+                }
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+            }
+        }
+
+        #endregion
+
+        #region Delete
+
+        private RelayCommand deleteCommand;
+
+        public RelayCommand DeleteCommand
+        {
+            get { return deleteCommand; }
+        }
+
+        public void Delete()
+        {
+            try
+            {
+                var isDeleted = ObjEmployeeService.Delete(CurrentEmployee.Id);
+
+                if (isDeleted)
+                {
+                    Message = "Employee deleted";
+                    LoadData();
+                }
+                else
+                {
+                    Message = "Delete failed";
+                }
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+            }
+        }
+
+        #endregion
+    }
+}
